b1a5ebcf4282bc4e5d4fe5a94991aade3f4835dd
BrynhildrW <56447252+BrynhildrW@users.noreply.github.com>
2022-07-07 16:41:43 +0800

    upload

---
+++ 1. socket网络编程/tcp_test.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+@ Author: Brynhildr Wu
+@ Email: brynhildrwu@gmail.com
+
+TCP/IP communication test
+
+update: 2022/7/5
+
+"""
+
+# %% load in modules
+from http import client
+import socket
+
+# %% tcp client
+# def tcp_client():
+#     # create tcp socket
+#     tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#     # target address
+#     server_ip = input('Please enter ip address:')  # str
+#     server_port = int(input('Please enter port:'))  # int
+#     server_addr = (server_ip, server_port)
+
+#     # connect to server
+#     tcp_client_socket.connect(server_addr)
+
+#     # send data
+#     send_data = input('Please enter your message:')
+#     tcp_client_socket.send(send_data.encode('gbk'))
+
+#     # close socket
+#     tcp_client_socket.close()
+
+
+# if __name__ == '__main__':
+#     tcp_client()
+
+
+# %% tcp server
+def tcp_server():
+    # create tcp socket
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # bind local address
+    local_addr = ('', 7788)
+    tcp_server_socket.bind(local_addr)
+
+    # listen(passive) mode
+    tcp_server_socket.listen(128)
+
+    continue_experiment = True
+    num = 0
+    while continue_experiment:
+        # wait for client and create a new socket for it
+        client_socket, client_addr = tcp_server_socket.accept()
+        print('Welcome new client: ' + str(client_addr))
+
+        while True:
+            # receive requirements from client
+            recv_data = client_socket.recv(1024)  # already got the target address, only return data
+            if not recv_data:  # client is closed | recv_data=None
+                print('Thank you! Waiting for next client...')
+                break
+            else:
+                msg = recv_data.decode('gbk')
+
+            # confirm data status
+            client_socket.send('Data received!'.encode('gbk'))
+            print(msg)
+        # finish communication
+        client_socket.close()
+        num += 1
+
+        # check server status
+        if num == 5:
+            continue_experiment = False
+            print('Up to limit')
+
+    # close server
+    tcp_server_socket.close()
+
+if __name__ == '__main__':
+    tcp_server()
+
---
+++ 1. socket网络编程/udp_test.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+@ Author: Brynhildr Wu
+@ Email: brynhildrwu@gmail.com
+
+UDP/IP communication test
+
+update: 2022/7/5
+
+"""
+
+# %% load in modules
+import socket
+
+# %% udp communication
+def send_label(udp):
+    dest_ip = input('Please enter ip address:')  # str
+    dest_port = int(input('Please enter port:'))  # int
+    predict_label = int(input('Please enter predicted label:'))  # int
+    udp.sendto(predict_label.encode('gbk'), (dest_ip, dest_port))  # or 'utf-8'
+
+def recv_label(udp):
+    predict_label = udp.recvfrom(1024)  # ((ip,port), msg)
+    print('%s:%s' % (str(predict_label[1]), str(predict_label[0].decode('gbk'))))
+
+def main():
+    # create udp socket
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # bind address
+    local_addr = ('', 8081)
+    udp_socket.bind(local_addr)  # only support local address
+
+    # begin communication
+    continue_experiment = True
+    while continue_experiment:
+        send_label(udp_socket)  # sending message
+        recv_label(udp_socket)  # receive message
+
+    # close socket
+    udp_socket.close()
+
+if __name__ == "__main__":
+    main()
+
+# %% tcp communication
+
+# create tcp socket
+
+
+# target address
+
+
+# connect to server
+
+
+# receive data from server
+
+
+# close socket
+
+if __name__ == '__main__':
+    main()
